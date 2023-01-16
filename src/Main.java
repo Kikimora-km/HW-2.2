768d9de536cf9564f00d22a514fdafd49e854355
Saidova Linda <kikimora.99.km@gmail.com>
2023-01-16 12:40:17 +0300

    add code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        int firstAccoutn = 50;
+        int addMoney = 1_100;
 
-        System.out.println( );
+        int bonus = addMoney > 1_000 ? addMoney / 100 : 0;
+
+        int finalAccount = firstAccoutn + addMoney + bonus;
+
+        System.out.println("Первоначальный счет: " + firstAccoutn);
+        System.out.println("Пополнение: " + addMoney);
+        System.out.println("Бонус: " + bonus);
+        System.out.println("Итоговый счет: " + finalAccount);
     }
 }
